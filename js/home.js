18ba510002d96777bf0db9479f99124c6f3e6fea
May <meifutoolate@gmail.com>
2012-10-16 17:27:29 +0800

    home page content

--- js/home.js
+++ js/home.js
@@ -10,5 +10,22 @@ jQuery(document).ready(function(){
         //controlNav: true,
         //controlNavThumbs: false
     //});
+
+	jQuery('.newsSwitch').click(function(){
+		console.log(jQuery(this).next('.newsContent'));
+		var newsContent = jQuery(this).next('.newsContent');
+		if(jQuery(this).hasClass('open')){
+
+			newsContent.hide();
+			jQuery(this).removeClass('open');
+
+		}else{
+
+			newsContent.show();
+			jQuery(this).addClass('open');
+
+		}
+		
+	});
 });
 
