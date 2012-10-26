67ad2f46f6040cda620efca4b80523fe3622b839
Hamm <hhuang@beefdaddy.com>
2012-10-26 18:13:02 +0800

    請看 demo.html

--- js/product.js
+++ js/product.js
@@ -5,31 +5,48 @@ jQuery(document).ready(function(){
         "height" : Geometry.getViewportHeight() + "px"
     });
 
+    jQuery("section").each(function(index, value){
+        jQuery(this).width(Geometry.getViewportWidth());
+        jQuery(this).height(Geometry.getViewportHeight());
+    });
+
+    // jQuery(".mm-warp").each(function(index, value){
+    //     jQuery(this).height(jQuery(this).parent().height());
+    // });
+
     jQuery(window).resize(function(){
         
-        jQuery("#product-wapper").css({
-            "width"  : Geometry.getViewportWidth()  + "px",
-            "height" : Geometry.getViewportHeight() + "px"
+        // jQuery("#product-wapper").css({
+        //     "width"  : Geometry.getViewportWidth()  + "px",
+        //     "height" : Geometry.getViewportHeight() + "px"
+        // });
+
+        // jQuery(".mm-warp").each(function(index, value){
+        //     jQuery(this).height(jQuery(this).parent().height());
+        // });
+
+        jQuery("section").each(function(index, value){
+            jQuery(this).width(Geometry.getViewportWidth());
+            jQuery(this).height(Geometry.getViewportHeight());
         });
-
-
 
     });
 
     window.setInterval(function(){
         
-        // console.log('window.scrollTop = ' + $(window).scrollTop());
-        console.log('product-wapper width = ' + Geometry.getViewportWidth());
-        // console.log('product-wapper height = ' + Geometry.getViewportHeight());
+        console.log('window.scrollTop = ' + $(window).scrollTop());
+        // console.log('product-wapper width = ' + Geometry.getViewportWidth());
+        // // console.log('product-wapper height = ' + Geometry.getViewportHeight());
 
-        console.log(jQuery(".mm-wapper").css("padding"));
-        console.log(jQuery(".mm-wapper").css("width"));
+        // console.log(jQuery(".mm-wapper").css("padding"));
+        // console.log(jQuery(".mm-wapper").css("width"));
 
-        var VierPortWidth = Geometry.getViewportWidth();
-        var ContentWidth = jQuery(".mm-wapper").width();
+        // var VierPortWidth = Geometry.getViewportWidth();
+        // var ContentWidth = jQuery(".mm-wapper").width();
 
-        console.log(VierPortWidth - ContentWidth);
+        // console.log(VierPortWidth - ContentWidth);
 
+        // jQuery("#product-wapper").height(Geometry.getViewportHeight());
 
 
 
