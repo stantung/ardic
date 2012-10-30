ee21be461c37d9419ac492a0dd3c00342744c174
Hamm <hhuang@beefdaddy.com>
2012-10-30 14:19:27 +0800

    調整可視範圍

--- js/product.js
+++ js/product.js
@@ -7,20 +7,14 @@ jQuery(document).ready(function(){
 
     resize();
 
-    jQuery("#product-wrap").css({
+    jQuery(".product-wrap").css({
         "width"  : Geometry.getViewportWidth()  + "px",
         "height" : Geometry.getViewportHeight() + "px"
     });
 
     jQuery("section").each(function(index, value){
-
-
-
         // jQuery(this).width(Geometry.getViewportWidth());
         // jQuery(this).height(Geometry.getViewportHeight());
-
-        
-
     });
 
     // jQuery(".mm-warp").each(function(index, value){
@@ -33,12 +27,14 @@ jQuery(document).ready(function(){
 
     function resize () {
 
+
+
         if (Geometry.getViewportWidth() > 1600) {
             PRODUCT_WAPPER_MARGIN_LEFT  = ((Geometry.getViewportWidth() - 1600) / 2);
-            // PRODUCT_WAPPER_MARGIN_RIGHT = ((1400 - Geometry.getViewportWidth()) / 2);
+            // PRODUCT_WAPPER_MARGIN_RIGHT = ((Geometry.getViewportWidth() - 1600) / 2);
             // PRODUCT_WAPPER_MARGIN_TOP    = ((1000 - Geometry.getViewportHeight()) / 2);
             // PRODUCT_WAPPER_MARGIN_BOTTOM = ((1000 - Geometry.getViewportHeight()) / 2);
-           jQuery("#product-wrap").css({
+           jQuery(".product-wrap").css({
                 "width"  : Geometry.getViewportWidth()  + "px",
                 "height" : Geometry.getViewportHeight() + "px",
                 "padding-top"    : PRODUCT_WAPPER_MARGIN_TOP    + "px",
@@ -48,12 +44,12 @@ jQuery(document).ready(function(){
             });
         } else {
 
-            PRODUCT_WAPPER_MARGIN_TOP    = 0;
-            PRODUCT_WAPPER_MARGIN_LEFT  = ((Geometry.getViewportWidth() - 1600) / 2);
+            PRODUCT_WAPPER_MARGIN_TOP    = ((Geometry.getViewportHeight() - 1600) / 2);
+            PRODUCT_WAPPER_MARGIN_LEFT   = ((Geometry.getViewportWidth() - 1600) / 2);
             PRODUCT_WAPPER_MARGIN_RIGHT  = 0;
             PRODUCT_WAPPER_MARGIN_BOTTOM = 0;
 
-            jQuery("#product-wrap").css({
+            jQuery(".product-wrap").css({
                 "width"  : Geometry.getViewportWidth()  + "px",
                 "height" : Geometry.getViewportHeight() + "px",
                 "margin-top"    : PRODUCT_WAPPER_MARGIN_TOP    + "px",
@@ -65,28 +61,20 @@ jQuery(document).ready(function(){
                 "padding-right"  : "0px",
                 "padding-bottom" : "0px"
             });
+
+            jQuery(".content-wrap").each(function(index, value){
+                jQuery(this).css({
+                    "top" : (Geometry.getViewportHeight() / 2) - 250 + "px"
+                });
+                console.log((Geometry.getViewportHeight() / 2) - 250 + "px");
+            });
+
+
         }
     }
 
     window.setInterval(function(){
-        
-        console.log('window.scrollTop = ' + $(window).scrollTop());
-        // console.log('Geometry.getViewportWidth() = ' + Geometry.getViewportWidth());
-
-        // // console.log('product-wapper height = ' + Geometry.getViewportHeight());
-
-        // console.log(jQuery(".mm-wapper").css("MARGIN"));
-        // console.log(jQuery(".mm-wapper").css("width"));
-
-        // var VierPortWidth = Geometry.getViewportWidth();
-        // var ContentWidth = jQuery(".mm-wapper").width();
-
-        // console.log(VierPortWidth - ContentWidth);
-
-        // jQuery("#product-wapper").height(Geometry.getViewportHeight());
-
-
-
+        // console.log('window.scrollTop = ' + $(window).scrollTop());
     }, 1000);
 
 });
